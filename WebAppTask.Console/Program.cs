6b6d73306d577a075e95ec2cb078e7463fb197a9
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2024-05-08 12:47:34 +0300

    Add project files.

---
+++ WebAppTask.Console/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WebAppTask.Console
+{
+    internal class Program
+    {
+         static async Task Main(string[] args)
+        {
+            do
+            {
+                AddLog("App is running... ");
+
+                System.Console.WriteLine("Request Type (Sync = 0, Async =1 ): ");
+
+                int requestType = int.Parse(System.Console.ReadLine());
+
+                System.Console.WriteLine("How mant request : ");
+
+                int requestcount=int.Parse(System.Console.ReadLine());
+
+                var tasks=requestType==0 ? GetSyncTasks(requestcount) : GetAsyncTasks(requestcount);
+
+                var sw = Stopwatch .StartNew();
+
+                await Task.WhenAll(tasks);
+
+                sw.Stop();
+                AddLog($"Total Time : {sw.ElapsedMilliseconds} MS");
+
+
+            } while (System.Console.ReadKey().Key== System.ConsoleKey.R);
+
+        }
+
+        public static IEnumerable<Task> GetSyncTasks(int howMany)
+        {
+            var result = new List<Task>();
+            var clinet = new WebApiClient();
+            for (int i = 0; i < howMany; i++)
+            {
+                result.Add(clinet.CallSync());
+            }
+            return result;
+        }
+
+
+        public static IEnumerable<Task> GetAsyncTasks(int howMany)
+        {
+            var result = new List<Task>();
+            var clinet = new WebApiClient();
+            for (int i = 0; i < howMany; i++)
+            {
+                result.Add(clinet.CallAsync());
+            }
+            return result;
+        }
+
+        private static void AddLog(string logStr)
+        {
+            logStr = $"[{DateTime.Now:dd.MM.yyyy HH:mm:ss}] - {logStr}";
+            System.Console.WriteLine (logStr);
+        }
+    }
+}
---
+++ WebAppTask.Console/WebApiClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppTask.Console
+{
+    public class WebApiClient
+    {
+        private const string url = "https://localhost:5001/api/test/";
+
+        public async Task CallAsync()
+        {
+            HttpClient httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+            await httpClient.GetAsync("Async");
+        }
+
+
+        public async Task CallSync()
+        {
+            HttpClient httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+            await httpClient.GetAsync("Sync");
+        }
+    }
+}
